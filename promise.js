8ab7267071bd0869c7dbc57a9ad881b66b3483c9
Anastasiya Atroshchenko <belle.nastja@gmail.com>
2021-07-09 19:41:09 +0300

    new katas

--- promise.js
+++ promise.js
@@ -130,3 +130,42 @@ let numbers = [2, 3, 4]
 // AlexanderKhodaryonok
 //marina
 
+
+
+
+
+CODEWARS
+
+№1
+Function receive a two-dimensional square array of random integers. On the main diagonal, all the negative integers must be changed to 0, while the others must be changed to 1 (Note: 0 is considered non-negative, here).
+
+
+function matrix(array) {
+  
+    for(let i=0; i<array.length; i++){
+
+     if(array[i][i]>=0){
+       array[i][i]=1
+     } else {array[i][i]=0}
+    }
+    return array
+  
+}
+
+matrix([
+  [-1,  4, -5, -9,  3 ],
+  [ 6, -4, -7,  4, -5 ],
+  [ 3,  5,  0, -9, -1 ],
+  [ 1,  5, -7, -8, -9 ],
+  [-3,  2,  1, -5,  6 ]
+])
+
+Output array
+[
+  [ 0,  4, -5, -9,  3 ],
+  [ 6,  0, -7,  4, -5 ],
+  [ 3,  5,  1, -9, -1 ],
+  [ 1,  5, -7,  0, -9 ],
+  [-3,  2,  1, -5,  1 ]
+]
+
